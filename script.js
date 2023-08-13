29234a706b541bedec6c1243a0ee74946405e8a7
filipryznar <103883095+filipryznar@users.noreply.github.com>
2023-08-13 18:37:21 +0200

    add keyboard input and resolve bugs.

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ function operate(firstNum, operator, secondNum) {
       return multiply(firstNum, secondNum);
     case "÷":
       return divide(firstNum, secondNum);
+    case "*":
+      return multiply(firstNum, secondNum);
+    case "/":
+      return divide(firstNum, secondNum);
   }
 }
 
@@ -58,60 +62,130 @@ let num = {
 };
 
 //* Number buttons
+// Event listener for number buttons
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (num.operator == "") {
-      resetDisplay();
-      num.displayNumber = num.displayNumber + button.textContent;
-      num.firstNumber = Number(num.displayNumber);
-      number.textContent = Number(num.displayNumber);
-    } else {
-      num.secondNumber = num.secondNumber + button.textContent;
-      //firstNum = firstNum + button.textContent;
-      num.secondNumber = Number(num.secondNumber);
-      number.textContent = Number(num.secondNumber);
-    }
-    //TODO DELETE
-    console.log("first " + num.firstNumber);
-    console.log("second " + num.secondNumber);
-    console.log("display " + num.displayNumber);
-
-    displayNumber();
+    handleNumberClick(button.textContent);
   });
 });
+
+// Handle number button click
+function handleNumberClick(value) {
+  if (num.operator === "") {
+    resetDisplay();
+    if (value === "." && num.firstNumber.includes(".")) {
+      return updateNumberDisplay(Number(num.firstNumber));
+    }
+    num.firstNumber += value;
+    updateNumberDisplay(num.firstNumber);
+  } else {
+    if (value === "." && num.secondNumber.includes(".")) {
+      return updateNumberDisplay(Number(num.secondNumber));
+    }
+    num.secondNumber += value;
+    updateNumberDisplay(num.secondNumber);
+  }
+
+  // TODO: Delete
+  console.log("first " + num.firstNumber);
+  console.log("second " + num.secondNumber);
+  console.log("display " + num.displayNumber);
+
+  displayNumber();
+}
+
+// Update number display
+function updateNumberDisplay(content) {
+  number.textContent = content;
+}
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     if (num.operator == "") {
+//       resetDisplay();
+//       if (button.textContent === "." && num.firstNumber.includes(".")) {
+//         return (number.textContent = Number(num.firstNumber));
+//       }
+//       num.firstNumber += button.textContent;
+//       number.textContent = num.firstNumber;
+//     } else {
+//       if (button.textContent === "." && num.secondNumber.includes(".")) {
+//         return (number.textContent = Number(num.secondNumber));
+//       }
+//       num.secondNumber += button.textContent;
+//       number.textContent = num.secondNumber;
+//     }
+//     //TODO DELETE
+//     console.log("first " + num.firstNumber);
+//     console.log("second " + num.secondNumber);
+//     console.log("display " + num.displayNumber);
+
+//     displayNumber();
+//   });
+// });
 
 // Operator buttons
 operators.forEach((oper) => {
   oper.addEventListener("click", () => {
-    console.log(oper.textContent);
-    if (!num.result == "") {
-      num.firstNumber = num.result;
-      num.operator = oper.textContent;
-      lastNum.textContent = num.firstNumber + " " + num.operator;
-    } else if (!operator == "" && !!num.firstNumber == "") {
-      num.firstNumber = operate(
-        num.firstNumber,
-        num.operator,
-        num.secondNumber
-      );
-      num.operator = oper.textContent;
-      lastNum.textContent = num.firstNumber + " " + num.operator;
-    } else {
-      operator = oper.textContent;
-      num.operator = operator;
-      lastNum.textContent = num.firstNumber + " " + operator;
-      number.textContent = operator;
-    }
-
-    displayNumber();
+    handleOperatorClick(oper.textContent);
   });
 });
+
+// Handle operator button click
+function handleOperatorClick(operator) {
+  console.log(operator);
+
+  if (num.result !== "" && num.firstNumber === "") {
+    num.firstNumber = num.result;
+    num.operator = operator;
+    updateLastNumDisplay();
+  } else if (
+    num.operator !== "" &&
+    num.firstNumber !== "" &&
+    num.secondNumber !== ""
+  ) {
+    num.firstNumber = operate(
+      Number(num.firstNumber),
+      num.operator,
+      Number(num.secondNumber)
+    );
+    num.operator = operator;
+    updateLastNumDisplay();
+  } else {
+    num.operator = operator;
+    updateLastNumDisplay();
+    updateNumberDisplay(operator);
+  }
+
+  displayNumber();
+}
+
+// Update lastNum display
+function updateLastNumDisplay() {
+  lastNum.textContent = num.firstNumber + " " + num.operator;
+}
+
+// Update number display
+function updateNumberDisplay(content) {
+  const truncatedContent = truncateNumber(content, 10); // Truncate at a reasonable length
+  number.textContent = truncatedContent;
+}
+
+// Truncate or round the number to a specified length
+function truncateNumber(num, length) {
+  const numStr = num.toString();
+  if (numStr.length <= length) {
+    return numStr;
+  }
+  return numStr.slice(0, length);
+}
 //* Display
 function displayNumber() {
   number.classList.add("displayNumber");
   secondLine.appendChild(lastNum);
   display.appendChild(number);
 }
+
 function resetDisplay() {
   lastNum.textContent = "";
   number.textContent = 0;
@@ -129,6 +203,15 @@ clear.addEventListener("click", () => {
   num.result = "";
   resetDisplay();
 });
+
+function clearButtorn() {
+  num.firstNumber = "";
+  num.operator = "";
+  num.secondNumber = "";
+  num.displayNumber = "";
+  num.result = "";
+  resetDisplay();
+}
 
 //Delete button
 delButton.addEventListener("click", () => {
@@ -149,6 +232,9 @@ function delNumber() {
 
 // Calculation button
 calcul.addEventListener("click", () => {
+  calculation();
+});
+function calculation() {
   if (num.secondNumber == 0 && num.operator == "÷") {
     alert("Error: division by zero");
     num.firstNumber = "";
@@ -162,7 +248,11 @@ calcul.addEventListener("click", () => {
     !num.operator == "" &&
     !num.secondNumber == ""
   ) {
-    num.result = operate(num.firstNumber, num.operator, num.secondNumber);
+    num.result = operate(
+      Number(num.firstNumber),
+      num.operator,
+      Number(num.secondNumber)
+    );
     lastNum.textContent =
       num.firstNumber + " " + num.operator + " " + num.secondNumber + " =";
     console.log(num.result);
@@ -171,9 +261,76 @@ calcul.addEventListener("click", () => {
 
     //resetDisplay();
     displayNumber();
-    num.firstNumber = 0;
+    num.firstNumber = "";
     num.operator = "";
-    num.secondNumber = 0;
-    return;
+    num.secondNumber = "";
   }
+}
+
+// Allow keybard input
+const allowedKeys = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9", // Numeric keys
+  ".",
+  "+",
+  "-",
+  "*",
+  "/",
+  "Enter",
+  "=",
+  "Backspace",
+  "c",
+];
+const allowedOpers = ["+", "-", "*", "/"];
+const allowedNums = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9", // Numeric keys
+  ".",
+];
+
+// Add a keyboard event listener to the document
+document.addEventListener("keydown", (event) => {
+  const keyPressed = event.key;
+
+  // Check if the pressed key is in the allowedKeys array
+  if (!allowedKeys.includes(keyPressed)) {
+    event.preventDefault(); // Prevent the default action of the key
+    return; // Exit the function
+  }
+  if (keyPressed === "Enter" || keyPressed === "=") {
+    calculation();
+  } else if (keyPressed === "Backspace") {
+    if (num.secondNumber == "") {
+      delNumber();
+      number.textContent = num.firstNumber;
+      displayNumber();
+    } else {
+      num.secondNumber = Number(num.secondNumber.toString().slice(0, -1));
+      number.textContent = num.secondNumber;
+      displayNumber();
+    }
+  } else if (allowedOpers.includes(keyPressed)) {
+    handleOperatorClick(keyPressed);
+  } else if (allowedNums.includes(keyPressed)) {
+    handleNumberClick(keyPressed);
+  } else if (keyPressed === "c") {
+    clearButtorn();
+  }
+  console.log("Valid key pressed: " + keyPressed);
 });
